949632efe176309819e7ba63a9045e3d5d835a15
yacine <yace08@hotmail.fr>
2020-03-25 14:31:38 +0100

    div2

--- div2index.php
+++ div2index.php
@@ -8,7 +8,59 @@
 </head>
 <body>
     <header>
-<h1> les gestes pour limiter la transmission </h1>
+    <h1> le cocoronavirus </h1>
+<section>  
+<h2> Qu’est-ce que le nouveau coronavirus ?</h2>
+<p> Les coronavirus (surnommés CoV) sont une famille de virus plus ou moins sévères : selon le site 
+du gouvernement, ils peuvent provoquer de simples rhumes ou des pathologies plus lourdes telles que le syndrome respiratoire du Moyen-Orient (MERS) et le syndrome respiratoire aigu sévère (SRAS). Aujourd’hui, on en connaît six espèces. 
+Le nouveau coronavirus découvert en 2019 a été baptisé SARS-CoV-2 et la maladie qu’il entraîne, CoVid-19.</p>
+</secton>
+
+<section> 
+<h2>Quel est le mode de transmission ?</h2>
+<p> La transmission interhumaine, c’est-à-dire de personne à personne, a rapidement été confirmée. 
+La maladie se transmet par les voies respiratoires,
+ par les postillons (éternuements, toux). Le gouvernement explique que l’“on considère qu’un contact étroit avec 
+ une personne malade est nécessaire pour transmettre la maladie : même lieu de vie, contact direct à moins d’un mètre lors d’une toux, d’un éternuement ou une discussion en l’absence de mesures de protection".
+
+Une  récente étude chinoise publiée dans The Lancet a par
+ ailleurs montré que la durée moyenne de l'excrétion virale, définie comme l'expulsion des particules
+  virales du corps, était de 20 jours chez des personnes considérées comme guéries de
+   l'infection à CoVid-19, c'est-à-dire ne présentant plus de symptômes. Chez les 54 personnes décédées étudiées, le virus était détectable du début de la maladie jusqu'à leur mort.
+
+Néanmoins, le directeur général de l’Organisation mondiale de la santé
+ (OMS) Tedros Adhanom Ghebreyesus a déclaré le 3 mars 2020 qu'il est peu probable 
+ que les porteurs du virus asymptomatiques soient très contagieux : "Avec la grippe, les personnes infectées mais asymptomatiques sont des moteurs de transmission importants, ce qui ne semble pas être le cas avec le CoVid-19. Des données chinoises montrent que seulement 1 % des cas rapportés sont asymptomatiques, et la plupart des cas développent des symptômes dans les deux jours suivant l’infection".
+
+Aucun autre mode de transmission n’a à ce jour été identifié. Il est
+ donc peu probable que la maladie se transmette par l’eau, ou encore par les colis en provenance de Chine, comme 
+ le craignaient certaines personnes.</p>
+ </section>
+
+ <section>
+<<h2>Quels sont les symptômes du coronavirus COVID-19 ?</h2>
+<p>La société américaine Web MD, qui fournit des informations de santé, 
+a repris une analyse de l'Organisation Mondiale de la Santé. Celle-ci a été menée auprès de 55 924 cas confirmés en Chine, pour en savoir plus sur le virus et ses symptômes. 
+Voici les signes les plus fréquents et le pourcentage de personnes qui en ont souffert, d'après l'étude :
+
+Fièvre : 88% ; 
+Toux sèche : 68% ; 
+Fatigue: 38% ;
+Expectorations ou flegme épais des poumons: 33% ;
+Essoufflement: 19% ; 
+Douleurs osseuses ou articulaires: 15% ;
+Maux de gorge: 14% ;
+Maux de tête: 14% ;
+Frissons: 11% ;
+Nausées ou vomissements: 5% ;
+Nez bouché: 5% ;
+Diarrhée: 4% ;
+Toux de sang: 1% ;
+Yeux gonflés: 1%.
+</p>
+</section>
+
+
     
     
     
