79788a7487c6440a79ea2ded3d419ba5a230003a
Yvan0112 <iryvan1@live.fr>
2020-03-25 15:08:37 +0100

    look du site

---
+++ dev1/assets/js/app.js
@@ -0,0 +1,11 @@
+
+
+
+  // référence éléments du DOM
+const menu = document.querySelector("#menu");
+const sliderMenu = document.querySelector(".slide");
+
+// écouteur sur le click
+menu.addEventListener("click", function() {
+  sliderMenu.classList.toggle("slide");
+});
