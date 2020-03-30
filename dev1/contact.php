f64a2397c4082cb0fd1b1d1209df16e9f931cd3b
Yvan0112 <iryvan1@live.fr>
2020-03-30 16:54:42 +0200

    ajout du contenu

---
+++ dev1/contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet2Y</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<h1>"STOP COVIRUS"</h1>
+    <nav class=slide>
+            <ul>
+                <li><a href="accueil.php">Accueil</a></li>
+                <li><a href="symptomes.php">Symptômes</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div id="menu">Menu</div>
+        </nav>
+        <img src="assets/img/covid.jpg" alt="covid">
+        <div class="item-file">
+            <h3><strong>Numéros utiles</strong></h3>
+            <p>Un numéro vert répond à vos questions sur le Coronavirus COVID-19 en permanence, 24h/24 et 7j/7 : 0 800 130 000.</p>
+
+            <p>Si vous avez des difficultés pour entendre ou parler par téléphone en raison d’un handicap, vous pouvez vous rendre sur l'espace de contact ouvert dédié aux personnes sourdes, malentendantes ou aveugles.</p>
+
+            <p>Vous pouvez également vous tenir informé sur le compte du Gouvernement sur WhatsApp en cliquant ici, ou en enregistrant le numéro "07 55 53 12 12" au nom de Gouvernement puis en envoyant à ce contact un message sur l'application pour commencer la discussion.</p>
+            <p><strong>Attention, la plateforme téléphonique et le bot WhatsApp ne dispensent pas de conseils médicaux.</strong></p>
+            <p>Si j’ai des symptômes (toux, fièvre) qui me font penser au COVID-19 : je reste à domicile, j’évite les contacts, j’appelle un médecin avant de me rendre à son cabinet ou j’appelle le numéro de permanence de soins de ma région. Je peux également bénéficier d’une téléconsultation. Si les symptômes s’aggravent avec des difficultés respiratoires et signes d’étouffement, j'appelle le SAMU (15) ou j'envoie un message au numéro d’urgence pour les sourds et malentendants (114).</p>
+
+
+
+        <script src="assets/js/app.js"></script>
+    
+</body>
+</html>
---
+++ dev1/symptomes.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet2Y</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<h1>"STOP COVIRUS"</h1>
+    <nav class=slide>
+            <ul>
+                <li><a href="accueil.php">Accueil</a></li>
+                <li><a href="symptomes.php">Symptômes</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div id="menu">Menu</div>
+        </nav>
+        <img src="assets/img/covid.jpg" alt="covid">
+        <h2>Quels sont les symptômes du coronavirus COVID-19 ?</h2>
+<p>La société américaine Web MD, qui fournit des informations de santé, 
+a repris une analyse de l'Organisation Mondiale de la Santé. Celle-ci a été menée auprès de 55 924 cas confirmés en Chine, pour en savoir plus sur le virus et ses symptômes. 
+Voici les signes les plus fréquents et le pourcentage de personnes qui en ont souffert, d'après l'étude :
+Fièvre : 88% ; 
+Toux sèche : 68% ; 
+Fatigue: 38% ;
+Expectorations ou flegme épais des poumons: 33% ;
+Essoufflement: 19% ; 
+Douleurs osseuses ou articulaires: 15% ;
+Maux de gorge: 14% ;
+Maux de tête: 14% ;
+Frissons: 11% ;
+Nausées ou vomissements: 5% ;
+Nez bouché: 5% ;
+Diarrhée: 4% ;
+Toux de sang: 1% ;
+Yeux gonflés: 1%.
+Par ailleurs, le directeur général de la Santé Jérôme Salomon a expliqué dans un point presse donné le 20 mars que le conseil professionnel des spécialistes d’oto-rhino-laryngologie (ORL) observait chez les personnes atteintes de CoVid-19 une "recrudescence des cas d’anosmie brutale, qui correspond à la disparition brutale de l’odorat, sans obstruction du nez, sans écoulement et qui peut donc survenir de façon isolée". Cette perte d'odorat peut en revanche s'accompagner d'une perte de goût (agueusie), "surtout chez les sujets les plus jeunes", souligne un tweet du ministère de la Santé. Bien qu'il s'agisse de symptômes rares, ce dernier invite les personnes e
+souffrant à demander un avis médical par téléphone "pour savoir si oui ou non un traitement spécifique est nécessaire".
+</p>
+
+
+
+
+
+
+        <script src="assets/js/app.js"></script>
+    
+</body>
+</html>
