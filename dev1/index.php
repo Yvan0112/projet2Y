9f482f6139cafea36927dfcf5c1a8c1d7f96049c
Yvan0112 <iryvan1@live.fr>
2020-04-10 12:21:14 +0200

    mise en onepage

--- dev1/index.php
+++ dev1/index.php
@@ -8,25 +8,25 @@
     <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
-<body>
+<body id = "haut">
     <header>
     
     <nav class=slide>
             <ul>
-                <li><a href="index.php">Menu Principal</a></li>
-                <li><a href="accueil.php">Accueil</a></li>
-                <li><a href="symptomes.php">Informations essentielles</a></li>
-                <li><a href="attestation.php">Attestation dérogatoire</a></li>
-                <li><a href="contact.php">Contact et N° utiles</a></li>
+                
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="#Symptomes">Informations essentielles</a></li>
+                <li><a href="#attestation">Attestation dérogatoire</a></li>
+                <li><a href="#contact">Contact et N° utiles</a></li>
             </ul>
         
 </header>
-<main id = "haut">
+<main>
 <h1>"STOP COVIRUS"</h1>
 </nav>
         <div id="menu">Menu</div>
         </nav>
-        
+<section id = "accueil">
         <div id="title-top">
         <h2><span>Informations</span>
         CORONAVIRUS</h2>
@@ -44,6 +44,160 @@
             <p> Les coronavirus sont des virus connus depuis longtemps chez l’homme ainsi que chez les animaux. Ils provoquent la plupart du temps des maladies bénignes comme le rhume. Cependant, certains coronavirus entrainent des maladies plus graves telles que le syndrome respiratoire aigu sévère (SRAS) ou bien le syndrome respiratoire du Moyen-Orient (MERS) apparu en 2012 en Arabie Saoudite.
 
 Le virus qui sévit actuellement est dénommé Covid-19. Il s’agit d'un nouveau coronavirus qui a été découvert le 7 janvier, après que plusieurs cas de pneumonies virales soient apparus en Chine dans la ville de Wuhan. Les premières personnes touchées par le Covid-19 s'étaient rendues au marché de Wuhan, où des animaux vivants étaient vendus. La piste de la zoonose (maladie qui se transmet de l'animal à l'homme) est donc privilégiée pour définir l'origine du virus, bien qu'elle ne soit pas encore confirmée.</p>
+</section>
+<section id = "Symptomes">
+<h2>Qu'est-ce que le nouveau coronavirus?</h2>
+        <p>Les coronavirus (surnommés CoV) sont une famille de virus plus ou moins sévères : selon le site du gouvernement, ils peuvent provoquer de simples rhumes ou des pathologies plus lourdes telles que le syndrome respiratoire du Moyen-Orient (MERS) et le syndrome respiratoire aigu sévère (SRAS). Aujourd’hui, on en connaît six espèces. Le nouveau coronavirus découvert en 2019 a été baptisé SARS-CoV-2 et la maladie qu’il entraîne, CoVid-19.</p>
+        
+        <h2>Quels sont les symptômes du coronavirus COVID-19 ?</h2>
+        <img src="assets/img/symptomes.jpg" alt="symptomes">
+        <div>
+        
+<p>La société américaine Web MD, qui fournit des informations de santé, 
+a repris une analyse de l'Organisation Mondiale de la Santé. Celle-ci a été menée auprès de 55 924 cas confirmés en Chine, pour en savoir plus sur le virus et ses symptômes.</p> 
+<p>Voici les signes les plus fréquents et le pourcentage de personnes qui en ont souffert, d'après l'étude :</p>
+<p>Fièvre : 88% ; </p>
+<p>Toux sèche : 68% ; </p>
+<p>Fatigue: 38% ;</p>
+<p>Expectorations ou flegme épais des poumons: 33% ;</p>
+<p>Essoufflement: 19% ; </p>
+<p>Douleurs osseuses ou articulaires: 15% ;</p>
+<p>Maux de gorge: 14% ;</p>
+<p>Maux de tête: 14% ;</p>
+<p>Frissons: 11% ;</p>
+<p>Nausées ou vomissements: 5% ;</p>
+<p>Nez bouché: 5% ;</p>
+<p>Diarrhée: 4% ;</p>
+<p>Toux de sang: 1% ;</p>
+<p>Yeux gonflés: 1%.</p>
+<p>Par ailleurs, le directeur général de la Santé Jérôme Salomon a expliqué dans un point presse donné le 20 mars que le conseil professionnel des spécialistes d’oto-rhino-laryngologie (ORL) observait chez les personnes atteintes de CoVid-19 une "recrudescence des cas d’anosmie brutale, qui correspond à la disparition brutale de l’odorat, sans obstruction du nez, sans écoulement et qui peut donc survenir de façon isolée". Cette perte d'odorat peut en revanche s'accompagner d'une perte de goût (agueusie), "surtout chez les sujets les plus jeunes", souligne un tweet du ministère de la Santé. Bien qu'il s'agisse de symptômes rares, ce dernier invite les personnes e
+souffrant à demander un avis médical par téléphone "pour savoir si oui ou non un traitement spécifique est nécessaire".
+</p>
+</div>
+<h2>Quel est le mode de transmission?</h2>
+<p>La transmission interhumaine, c’est-à-dire de personne à personne, a rapidement été confirmée. La maladie se transmet par les voies respiratoires, par les postillons (éternuements, toux). Le gouvernement explique que l’“on considère qu’un contact étroit avec une personne malade est nécessaire pour transmettre la maladie : même lieu de vie, contact direct à moins d’un mètre lors d’une toux, d’un éternuement ou une discussion en l’absence de mesures de protection". Une récente étude chinoise publiée dans The Lancet a par ailleurs montré que la durée moyenne de l'excrétion virale, définie comme l'expulsion des particules virales du corps, était de 20 jours chez des personnes considérées comme guéries de l'infection à CoVid-19, c'est-à-dire ne présentant plus de symptômes. Chez les 54 personnes décédées étudiées, le virus était détectable du début de la maladie jusqu'à leur mort. Néanmoins, le directeur général de l’Organisation mondiale de la santé (OMS) Tedros Adhanom Ghebreyesus a déclaré le 3 mars 2020 qu'il est peu probable que les porteurs du virus asymptomatiques soient très contagieux : "Avec la grippe, les personnes infectées mais asymptomatiques sont des moteurs de transmission importants, ce qui ne semble pas être le cas avec le CoVid-19. Des données chinoises montrent que seulement 1 % des cas rapportés sont asymptomatiques, et la plupart des cas développent des symptômes dans les deux jours suivant l’infection". Aucun autre mode de transmission n’a à ce jour été identifié. Il est donc peu probable que la maladie se transmette par l’eau, ou encore par les colis en provenance de Chine, comme le craignaient certaines personnes.</p>
+<img src="assets/img/gestes.jpg" alt="gestes">
+
+</section>
+<section id = "attestation">
+<h2>ATTESTATION DE DEPLACEMENT DEROGATOIRE</h2>
+            <span>En application de l’article 3 du décret du    23 mars 2020    prescrivant les mesures générales nécessaires pour faire face à l’épidémie de Covid19 dans le cadre de l’état d’urgence sanitaire</span>
+            <p> Je soussigné(e),</p>
+            <form action="" method="POST">
+                <span>Mme/M :</span>
+                <input type="text" name="nom" required placeholder="votre nom et prénom">
+                <span>Né(e) le :</span>
+                <input type="datetime" name="dateNaissance" required placeholder="Date de Naissance">
+                <span>Demeurant : </span>
+                <textarea name="adresse" cols="60" rows="6" required placeholder="votre adresse"></textarea>
+                <p>certifie que mon déplacement est lié au motif suivant (cocher la case) autorisé par l’article 3 du décret du 23 mars 2020 prescrivant les mesures générales nécessaires pour faire face à l’épidémie de Covid19 dans le cadre de l’état d’urgence sanitaire.</p>
+                <h3>cocher la raison de votre déplacement</h3>
+
+                <label>
+                    <input type="radio" name="raison" required value="travail">
+                    <span>Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle, lorsqu’ils sont indispensables à l’exercice d’activités ne pouvant être organisées sous forme de télétravail ou déplacements professionnels ne pouvant être différés</span>
+                </label>
+
+                <label>
+                    <input type="radio" name="raison" required value="courses alimentaires">
+                    <span>Déplacements pour effectuer des achats de fournitures nécessaires à l’activité professionnelle et des achats de première nécessité dans des établissements dont les activités demeurent autorisées (liste sur gouvernement.fr).</span>
+                </label>
+
+                <label>
+                    <input type="radio" name="raison" required value="necessité médicale">
+                    <span>Consultations et soins ne pouvant être assurés à distance et ne pouvant être différés ; consultations et soins des patients atteints d'une affection de longue durée.</span>
+                </label>
+
+                <label>
+                    <input type="radio" name="raison" required value="aide aux proches">
+                    <span>Déplacements pour motif familial impérieux, pour l’assistance aux personnes vulnérables ou la garde d’enfants.</span>
+                </label>
+
+                <label>
+                    <input type="radio" name="raison" required value="sortie sport individuel">
+                    <span>Déplacements brefs,dans la limite d'une heure quotidienne et dans un rayon maximal d'un kilomètre autour du domicile,liés soit à l'activité physique individuelle des personnes, à l'exclusion de toute pratique sportive collective et de toute proximité avec d'autres personnes,soit à la promenade avec les seules personnes regroupées dans un même domicile, soit aux besoins des animaux de compagnie.</span>
+                </label>
+
+                <label>
+                    <input type="radio" name="raison" required value="convocation">
+                    <span>Convocation judiciaire ou administrative.</span>
+                </label>
+                <label>
+                    <input type="radio" name="raison" required value="mission">
+                    <span>Participation à des missions d'intérêt général sur demande de l'autorité administrative.</span>
+                </label>
+                <span>Fait à :</span>
+                <input type="text" name="lieu" required placeholder="votre lieu">
+                <span>Le :</span>
+                <input type="datetime" name="dateDeclaration" required placeholder="Date de déclaration">
+                    
+
+                <button type="submit">enregistrer ma déclaration</button>
+                <input type="hidden" name="identifiantFormulaire" value="formulaire">
+                <div class="confirmation">
+<?php
+
+require_once"php/functions.php";
+
+$identifiantFormulaire = filtrer("identifiantFormulaire");
+
+if ($identifiantFormulaire == "formulaire")
+{
+    $tabAssoColonneValeur = ["nom" => filtrer("nom"),
+                             "dateNaissance" => filtrer("dateNaissance"),
+                             "adresse" => filtrer("adresse"),
+                             "raison" => filtrer("raison"),
+                             "lieu" => filtrer("lieu"),
+                             "dateDeclaration" => filtrer("dateDeclaration"),
+                             ];
+    extract($tabAssoColonneValeur);
+    if ( $nom != ""
+        && $dateNaissance != ""
+        && $adresse != ""
+        && $raison != ""
+        && $lieu !="")
+        
+     { 
+        
+        $tabAssoColonneValeur["dateDeclaration"] = date("Y-m-d H:i:s");
+        
+        insererLigneSQL("formulaire", $tabAssoColonneValeur);
+
+        echo "votre déclaration est bien enregistrée. Le {$tabAssoColonneValeur["dateDeclaration"]}";
+    }
+    else
+    {
+        echo "VEUILLEZ FOURNIR TOUTES LES INFORMATIONS SVP...";
+    }
+
+}
+
+?>
+
+
+            </div>
+        </form>
+
+</section>
+<section id = "contact">
+<img src="assets/img/covid.jpg" alt="covid">
+        <div class="item-file">
+            <h3><strong>Numéros utiles</strong></h3>
+            <p>Un numéro vert répond à vos questions sur le Coronavirus COVID-19 en permanence, 24h/24 et 7j/7 : 0 800 130 000.</p>
+
+            <p>Si vous avez des difficultés pour entendre ou parler par téléphone en raison d’un handicap, vous pouvez vous rendre sur l'espace de contact ouvert dédié aux personnes sourdes, malentendantes ou aveugles.</p>
+
+            <p>Vous pouvez également vous tenir informé sur le compte du Gouvernement sur WhatsApp en cliquant ici, ou en enregistrant le numéro "07 55 53 12 12" au nom de Gouvernement puis en envoyant à ce contact un message sur l'application pour commencer la discussion.</p>
+            <p><strong>Attention, la plateforme téléphonique et le bot WhatsApp ne dispensent pas de conseils médicaux.</strong></p>
+            <p>Si j’ai des symptômes (toux, fièvre) qui me font penser au COVID-19 : je reste à domicile, j’évite les contacts, j’appelle un médecin avant de me rendre à son cabinet ou j’appelle le numéro de permanence de soins de ma région. Je peux également bénéficier d’une téléconsultation. Si les symptômes s’aggravent avec des difficultés respiratoires et signes d’étouffement, j'appelle le SAMU (15) ou j'envoie un message au numéro d’urgence pour les sourds et malentendants (114).</p>
+            <p>Pour plus de questions et d'éclaircissement, n'hésitez pas à nous en faire. Nous ne manquerons pas de vous contacter:</p>
+            <form id="contact" action="#contact" method="POST">
+        <input type="text" name="nom" placeholder="entrez votre nom">
+        <input type="email" name="email" placeholder="entrez votre email">
+        <textarea name="message" cols="80" rows="8" placeholder="entrez votre message"></textarea>
+        <button type="submit">envoyer votre message</button>
+    </form>
+</section>
 </main>
 <footer>
 
